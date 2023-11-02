0b2b33b33ea96652e67e343144484b9691ecb87d
mpierce <mattison.pierce@gmail.com>
2023-11-02 14:08:59 -0500

    dagger fix

--- app/src/main/java/nss/capstone/activity/CreateRecipeActivity.java
+++ app/src/main/java/nss/capstone/activity/CreateRecipeActivity.java
@@ -1,6 +1,15 @@
 package nss.capstone.activity;
 
+import nss.capstone.dynamodb.RecipeDao;
+
+import javax.inject.Inject;
+
 public class CreateRecipeActivity {
 
+    private final RecipeDao recipeDao;
 
+    @Inject
+    public CreateRecipeActivity(RecipeDao recipeDao) {
+        this.recipeDao = recipeDao;
+    }
 }
